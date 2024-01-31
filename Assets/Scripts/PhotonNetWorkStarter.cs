55f49bf173124a9eb7c3d98ddd693a5dd8ee9dbb
damakoo <71433895+damakoo@users.noreply.github.com>
2024-02-01 07:47:23 +0900

    add transport object

--- Assets/Scripts/PhotonNetWorkStarter.cs
+++ Assets/Scripts/PhotonNetWorkStarter.cs
@@ -4,24 +4,28 @@ using Mirror;
 public class PhotonNetWorkStarter: MonoBehaviour
 {
     [SerializeField] DecideHostorClient _DecideHostorClient;
-
+    private NetworkManager _networkManager;
+    private void Start()
+    {
+        _networkManager = GetComponent<NetworkManager>();
+    }
 
     // ホストボタン押下時に呼ばれる
     public void OnHostStart()
     {
-        GetComponent<NetworkManager>().StartHost();
+        _networkManager.StartHost();
     }
 
     // クライアントボタン押下時に呼ばれる
     public void OnClientStart()
     {
-        GetComponent<NetworkManager>().networkAddress = "localhost"; // IP指定
-        GetComponent<NetworkManager>().StartClient();
+        _networkManager.networkAddress = "localhost"; // IP指定
+        _networkManager.StartClient();
     }
 
     // セーバーボタン押下時に呼ばれる
     public void OnServerStart()
     {
-        GetComponent<NetworkManager>().StartServer();
+        _networkManager.StartServer();
     }
 }
